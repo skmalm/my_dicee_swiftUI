ee62a4ac5f55c680a69c41670274976bae4e2619
Sebastian Malm <skmalm@gmail.com>
2020-04-18 13:22:39 -0400

    Initial Commit

---
+++ my_dicee_swiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  my_dicee_swiftUI
+//
+//  Created by Sebastian Malm on 4/18/20.
+//  Copyright © 2020 SebastianMalm. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
